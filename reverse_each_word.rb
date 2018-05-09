20dedfa7f21bbd87db0ec00eb7bf65ef2d6584e6
marjutubli@gmail.com <marjutubli@gmail.com>
2018-05-09 11:57:54 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(word)
   word_arr = word.split(" ")
   reversed_arr = []
-  word_arr.collect{ |i| 
+  word_arr.collect!{ |i| 
   x = i.reverse}
   new_sentence = word_arr.join(" ")
 end
